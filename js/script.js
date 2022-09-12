7f08d2f0cbe1bf5bcb106676d36f0abfafe25268
alemonacoo <ale.monaco@outlook.com>
2022-09-12 14:39:21 +0200

    Excercise + Bonus Complete!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("JS OK!");
+
+const app = new Vue(
+    {
+        el: '#root',
+        data:{
+            message: 'Hello World!',
+            url: 'https://picsum.photos/200/300',
+        }
+
+
+})
